3a50ed64152c5d53f04a90326ee1295175ea1b7f
clement3872 <clement.noisy78@gmail.com>
2024-12-15 02:45:29 +0100

    pwd: id_exists

--- exo_TP/password_cpp/user.cpp
+++ exo_TP/password_cpp/user.cpp
@@ -2,16 +2,37 @@
 #include "password.hpp"
 
 User::User(int id, Password &password): id(id), password(password) {
-  this.is_logged_in = false;
+  (*this).is_logged_in = false; // same as `this->is_logged_in`
 }
 
-bool does_id_exits(std::string) {
-  std::ifstream file("filename.txt");
+std::string extract_id(std::string line) {
+	int marker = -1;
+	int i = 0;
+	for (int i = 0; i < line.length(); i++) {
+		if (line[i]=='|') {
+			marker = i;
+			break;
+		}
+	}
+
+	if (marker == -1) {
+		return "";
+	} else {
+		return line.substr(0,marker);
+	}
+}
+
+
+bool User::id_exits(std::string id_value) {
+  std::ifstream file("pwd.data");
+
   
   if (file.is_open()) {
     std::string line;
     while (std::getline(file, line)) {
-      std::cout << line << std::endl;
+	  if (extract_id(line)==id_value) {
+		  return true;
+	  }
     }
     file.close();
   } else {
@@ -21,19 +42,22 @@ bool does_id_exits(std::string) {
   return false;
 }
 
+bool User::id_exits(int id_value) {
+	return id_exits(std::to_string(id_value));
+}
 
 void User::save() {
   // In a file (or DB)
   // id|password (hashed) 
-  std::string content = std::to_string(this->id) + "|" + password.str() + "\n";
 
-  std::fstream file("filename.txt", file.out | file.app);
-  file << content;
-
-  std::cout << "Saved: " << content << std::endl;
-
-  does_id_exits(std::to_string(this->id));
-};
+  if (!id_exits(this->id)) {
+	  std::string content = std::to_string(this->id) + "|" + password.str() + "\n";
+	  std::fstream file("pwd.data", file.out | file.app);
+	  file << content;
+	  std::cout << "Saved: " << content << std::endl;
+  }
+  printf("Password already saved!\n");
+}
 
 int User::login(char *raw_password) {
   // Compare raw_password with password in DB
--- exo_TP/password_cpp/user.hpp
+++ exo_TP/password_cpp/user.hpp
@@ -10,6 +10,8 @@ class User {
         int id;
         Password &password;
         bool is_logged_in;
+	bool id_exits(int id_value);
+	bool id_exits(std::string id_value);
 
     public:
         explicit User(int id, Password &password);
